935973d6cede7f750f045cc99dfae4d4b1a61de1
tarekseguir <42669769+tarekseguir@users.noreply.github.com>
2019-11-07 19:51:36 +0100

    code of speech and facemark detection

--- face_detection/faceDetect.py
+++ face_detection/faceDetect.py
@@ -1,13 +1,16 @@
 import cv2
+import speech_recognition as sr
+import threading
 import numpy as np
 import matplotlib.pyplot as plt
 """
 nadia = cv2.imread("Nadia_Murad.jpg",0)
 denis = cv2.imread("Denis_Mukwege.jpg",0)
 solvay = cv2.imread("solvay_conference.jpg",0)
-#cv2.imshow("klj",solvay)
+cv2.imshow("klj",solvay)
 """
 face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_eye.xml")
 
 def detect_face(img):
     face_img=img.copy()
@@ -25,7 +28,7 @@ def adj_detect_face(img):
         cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
     return face_img
 
-eye_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_eye.xml")
+
 
 def detect_eye(img):
     eye_img=img.copy()
@@ -34,8 +37,8 @@ def detect_eye(img):
     for(x,y,w,h) in eye_rects:
         cv2.rectangle(eye_img,(x,y),(x+w,y+h),(255,255,255),10)
     return eye_img
-
-"""result = detect_eye(denis)#cet photo est indetectable(yeux) car la partie blanc dans l'ouiel est presque ''noir''(invisible)
+"""
+result = detect_eye(denis)#cet photo est indetectable(yeux) car la partie blanc dans l'ouiel est presque ''noir''(invisible)
 cv2.imshow('eye',result)
 result2 = detect_eye(nadia)
 cv2.imshow('eye2',result2)
@@ -43,13 +46,32 @@ result12 = detect_eye(solvay)
 cv2.imshow('eye12',result12)
 """
 #Cas d'un Video
-cap=cv2.VideoCapture(0)
-while True:
-    ret,frame=cap.read(0)
-    frame=detect_face(frame)
-    cv2.imshow('mlk',frame)
-    if cv2.waitKey(1)==27 :
-        break
-cap.release()
-cv2.destroyAllWndows()
+def face_detection():
 
+    cap=cv2.VideoCapture(0)
+    while True:
+        ret,frame=cap.read(0)
+        frame=detect_face(frame)
+        cv2.imshow('mlk',frame)
+        if cv2.waitKey(33) == ord('a') :
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+def speech_detection():
+    r = sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            print("phase 1:")
+            audio = r.listen(source)
+
+            try:
+                text = r.recognize_google(audio)
+                print(text)
+            except:
+                print("NAN")
+
+
+speech =  threading.Thread(target=speech_detection)
+face = threading.Thread(target=face_detection)
+speech.start()
+face.start()
---
+++ speech_to_text/speech.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+while True:
+    with sr.Microphone() as source:
+        print("phase 1:")
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+        except:
+            print("NAN")
